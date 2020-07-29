792e0f96d193952a6ee1883ec461bc70fc41e401
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2020-07-29 20:17:55 +0300

    Added the changes.

--- git-example/src/main/java/ro/tefacprogramator/m4/gitexample/controllers/CartPage.java
+++ git-example/src/main/java/ro/tefacprogramator/m4/gitexample/controllers/CartPage.java
@@ -1,7 +1,8 @@
 package ro.tefacprogramator.m4.gitexample.controllers;
 
 public class CartPage {
-    public static void main(String[] args) {
+
+    public static void main2(String[] args) {
         System.err.println();
     }
     public static void sayHelloFromCart2(String[] args) {
@@ -9,8 +10,9 @@ public class CartPage {
         System.err.println("SAdsadsadsa");
         System.err.println("SAdsadsadsa");
     }
-
-    public static void main2(String[] args) {
+    public static void main(String[] args) {
         System.err.println();
     }
+
+
 }

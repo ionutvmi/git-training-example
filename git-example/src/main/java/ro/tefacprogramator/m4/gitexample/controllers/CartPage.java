17b35a45fac455497a203b16f250f8315697246c
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2020-07-29 20:13:51 +0300

    Added some changes for the cart.

--- git-example/src/main/java/ro/tefacprogramator/m4/gitexample/controllers/CartPage.java
+++ git-example/src/main/java/ro/tefacprogramator/m4/gitexample/controllers/CartPage.java
@@ -5,6 +5,10 @@ public class CartPage {
         System.err.println();
     }
 
+    public void myMethodCart1() {
+        System.out.println("Asdsadsadsa");
+    }
+
     public static void main2(String[] args) {
         System.err.println();
     }

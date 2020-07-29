7d384695636e680667105eb0193b63ab32f74b96
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2020-07-29 20:14:52 +0300

    Added changes to the cart page 2.

--- git-example/src/main/java/ro/tefacprogramator/m4/gitexample/controllers/CartPage.java
+++ git-example/src/main/java/ro/tefacprogramator/m4/gitexample/controllers/CartPage.java
@@ -4,6 +4,11 @@ public class CartPage {
     public static void main(String[] args) {
         System.err.println();
     }
+    public static void sayHelloFromCart2(String[] args) {
+        System.err.println("SAdsadsadsa");
+        System.err.println("SAdsadsadsa");
+        System.err.println("SAdsadsadsa");
+    }
 
     public static void main2(String[] args) {
         System.err.println();

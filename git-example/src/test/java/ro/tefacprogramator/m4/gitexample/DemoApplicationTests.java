d22fe4f5a6bc1cbba596d3f840f5f08c515e7944
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2020-07-29 19:27:46 +0300

    Initial commmit.

---
+++ git-example/src/test/java/ro/tefacprogramator/m4/gitexample/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.tefacprogramator.m4.gitexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

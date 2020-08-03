d7162788096c8f74ddfbb00e992353455d3887fc
razvanexu <razvandf@gmail.com>
2020-08-03 16:36:25 +0300

    Modificat main, HomePageController si ProductListingPage

--- git-example/src/main/java/ro/tefacprogramator/m4/gitexample/controllers/HomepageController.java
+++ git-example/src/main/java/ro/tefacprogramator/m4/gitexample/controllers/HomepageController.java
@@ -4,10 +4,13 @@ package ro.tefacprogramator.m4.gitexample.controllers;
  * HomepageController
  */
 public class HomepageController {
+    
+    public String message = "Hello";
+
 
     public void sayHello() {
-        System.out.println("Hello");
-        System.out.println("Hello");
+        System.out.println(message);
+        System.out.println(message);
         System.out.println("HELLO");
         System.out.println("Hello");
         System.out.println("Hello");
